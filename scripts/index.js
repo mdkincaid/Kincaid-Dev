276bd389e8ee0bea34b0177b58c883146650673e
Matthew <15mkincaid@gmail.com>
2019-04-01 21:38:10 -0400

    testing javascript functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
-console.log('Hi from index.js');
+let textArea = document.getElementById('text-area');
+let clickMeBtn = document.getElementById('click-me');
+
+clickMeBtn.addEventListener('click', function() {
+    textArea.innerText = 'Hello World!';
+});
